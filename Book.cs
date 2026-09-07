effd282ed200742331fd56b89c95b915d42de39a
Hilal-Nur <hilal2204nur@gmail.com>
2026-09-07 19:03:12 +0300

    Create Book.cs

---
+++ Book.cs
@@ -0,0 +1,10 @@
+public class Book
+{
+    public string BookTitle { get; set; } = "";
+    public string Author { get; set; } = "";
+    public string Genre { get; set; } = "";
+    public int PageCount { get; set; }
+    public int PublicationYear{ get; set; }
+    public string Isbn { get; set; } = "";
+    public bool Borrowed { get; set; }
+}
